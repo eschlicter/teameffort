29aaadcdcec70de5e869370316c10aff8293290c
Emily Schlicter <35039378+eschlicter@users.noreply.github.com>
2018-06-29 14:39:39 -0700

    scaffolding and deployment

--- src/routes/static.js
+++ src/routes/static.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
-router.get("/", (req, res, next) => {
-    res.send("Welcome to Blocipedia");
-});
 
+const staticController = require("../controllers/staticController");
+
+router.get("/", staticController.index);
 module.exports = router;
