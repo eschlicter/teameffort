22d3d15689f903d93c3be3868ba8495189d01d3b
Emily Schlicter <35039378+eschlicter@users.noreply.github.com>
2018-07-03 14:31:44 -0700

    working on adding sign_up

---
+++ src/controllers/userController.js
@@ -0,0 +1,31 @@
+const userQueries = require("../db/queries.users.js");
+const passport = require("passport");
+
+
+module.exports = {
+
+  signUp(req, res, next){
+    res.render("users/sign_up");
+  },
+
+  create(req, res, next){
+    let newUser = {
+      username: req.body.username,
+      email: req.body.email,
+      password: req.body.password,
+      passwordConfirmation: req.body.passwordConfirmation
+    };
+    userQueries.createUser(newUser, (err, user) => {
+      if(err){
+        console.log(err);
+        req.flash("error", err);
+        res.redirect("/users/sign_up");
+      } else {
+        passport.authenticate("local")(req, res, () => {
+          req.flash("notice", "You've signed up!");
+          res.redirect("/");
+        })
+      }
+    });
+}
+}
